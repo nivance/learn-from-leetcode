f17560709e84aaffbf76a9da9d10a8d9ee40f585
nivance <limingjian110@163.com>
2020-04-22 19:34:16 +0800

    二叉树的最大深度

--- README.md
+++ README.md
@@ -4,4 +4,5 @@
 * 相同的数 [Same Tree](/src/main/java/learn/from/leetcode/tree/SameTree.java) (Easy)
 * 对称二叉树 [Symmetric Binary Tree](/src/main/java/learn/from/leetcode/tree/SymmetricBinaryTree.java) (Easy)
 * 二叉树的中序遍历 [Inorder Traversal](/src/main/java/learn/from/leetcode/tree/InorderTraversal.java) (Easy)
+* 二叉树的最大深度 [Max Depth](/src/main/java/learn/from/leetcode/tree/MaxDepth.java) (Easy)
 * 二叉树的层序遍历 [Level Order](/src/main/java/learn/from/leetcode/tree/LevelOrder.java) (medium)
---
+++ src/main/java/learn/from/leetcode/tree/MaxDepth.java
@@ -0,0 +1,48 @@
+package learn.from.leetcode.tree;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+import learn.from.leetcode.ds.TreeNode;
+
+/**
+ * @author nivance
+ * @from https://leetcode-cn.com/problems/maximum-depth-of-binary-tree/
+ * @desc 二叉树的最大深度
+ */
+public class MaxDepth {
+	
+	public static int maxDepth(TreeNode root) {
+		if (root != null) {
+			int depth = 0;
+			Deque<TreeNode> queue = new LinkedList<TreeNode>();
+			queue.offer(root);
+			while(!queue.isEmpty()){
+				depth++;
+				int size = queue.size();
+				for (int i = 0; i < size; i++) {
+					TreeNode node = queue.pop();
+					if (node.left != null) {
+						queue.offer(node.left);
+					}
+					if (node.right != null) {
+						queue.offer(node.right);
+					}
+				}
+				size = queue.size();
+			}
+			return depth;
+		}
+		return 0;
+    }
+
+//	public int maxDepth2(TreeNode root) {
+//		if (root == null) {
+//			return 0;
+//		}
+//		int leftDepth = maxDepth(root.left);
+//		int rightDepth = maxDepth(root.right);
+//		return Math.max(leftDepth, rightDepth) + 1;
+//	}
+	
+}
