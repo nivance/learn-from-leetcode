d9f41f27650ddeb689cd6b304ba117a01a6c61e5
nivance <limingjian110@163.com>
2020-04-23 17:08:38 +0800

    add IsBalancedTree

--- README.md
+++ README.md
@@ -29,5 +29,6 @@
 * 对称二叉树 [Symmetric Binary Tree](/src/main/java/learn/from/leetcode/tree/SymmetricBinaryTree.java) (Easy)
 * 二叉树的中序遍历 [Inorder Traversal](/src/main/java/learn/from/leetcode/tree/InorderTraversal.java) (Easy)
 * 二叉树的最大深度 [Max Depth](/src/main/java/learn/from/leetcode/tree/MaxDepth.java) (Easy)
-* 二叉树的层序遍历 [Level Order](/src/main/java/learn/from/leetcode/tree/LevelOrder.java) (medium)
 * 将有序数组转换为二叉搜索树 [Sorted Array To BST](/src/main/java/learn/from/leetcode/tree/SortedArrayToBST.java) (Easy)
+* 平衡二叉树 [Balanced Binary Tree](/src/main/java/learn/from/leetcode/tree/IsBalancedTree.java) (Easy)
+* 二叉树的层序遍历 [Level Order](/src/main/java/learn/from/leetcode/tree/LevelOrder.java) (medium)
---
+++ src/main/java/learn/from/leetcode/tree/IsBalancedTree.java
@@ -0,0 +1,74 @@
+package learn.from.leetcode.tree;
+
+import learn.from.leetcode.ds.TreeNode;
+
+/**
+ * @author nivance
+ * @from https://leetcode-cn.com/problems/balanced-binary-tree/
+ * @desc 给定一个二叉树，判断它是否是高度平衡的二叉树。<br/>
+ *       本题中，一棵高度平衡二叉树定义为：<br/>
+ *       一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过1。
+ */
+public class IsBalancedTree {
+
+	/**
+	 * 自顶向下的遍历(存在大量冗余计算, 可通过map存储每个节点的高度)
+	 * 
+	 * @param root
+	 * @return
+	 */
+	public boolean isBalanced(TreeNode root) {
+		if (root != null) {
+			return (Math.abs(depth(root.left) - depth(root.right)) <= 1) && isBalanced(root.left)
+					&& isBalanced(root.right);
+		}
+		return true;
+	}
+
+	private int depth(TreeNode root) {
+		if (root == null) {
+			return 0;
+		}
+		return 1 + Math.max(depth(root.left), depth(root.right));
+	}
+
+	/**
+	 * 自底向上（后序遍历）
+	 * 
+	 * @param root
+	 * @return
+	 */
+	public boolean isBalanced2(TreeNode root) {
+		return isBalanceTree(root).balanced;
+	}
+
+	private TreeInfo isBalanceTree(TreeNode root) {
+		if (root == null) {
+			return new TreeInfo(-1, true);
+		}
+		TreeInfo leftTreeInfo = isBalanceTree(root.left);
+		if (!leftTreeInfo.balanced) {
+			return new TreeInfo(-1, false);
+		}
+		TreeInfo rightTreeInfo = isBalanceTree(root.right);
+		if (!rightTreeInfo.balanced) {
+			return new TreeInfo(-1, false);
+		}
+		if (Math.abs(leftTreeInfo.height - rightTreeInfo.height) <= 1) {
+			return new TreeInfo(Math.max(leftTreeInfo.height, rightTreeInfo.height), true);
+		} else {
+			return new TreeInfo(-1, false);
+		}
+	}
+
+	class TreeInfo {
+		public final int height;
+		public final boolean balanced;
+
+		public TreeInfo(int height, boolean balanced) {
+			this.height = height;
+			this.balanced = balanced;
+		}
+	}
+
+}
