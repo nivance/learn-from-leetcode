5e0199bc0d8813f4ea1a7567d780df0fd51711c4
nivance <limingjian110@163.com>
2020-04-26 10:05:54 +0800

    supplement

--- src/main/java/learn/from/leetcode/tree/SortedArrayToBST.java
+++ src/main/java/learn/from/leetcode/tree/SortedArrayToBST.java
@@ -3,27 +3,29 @@ package learn.from.leetcode.tree;
 import learn.from.leetcode.ds.TreeNode;
 
 /**
- * @author nivance
- * @from https://leetcode-cn.com/problems/convert-sorted-array-to-binary-search-tree/
+ * @author nivance <br/>
+ * @from https://leetcode-cn.com/problems/convert-sorted-array-to-binary-search-tree/ <br/>
+ *       https://leetcode-cn.com/problems/minimum-height-tree-lcci/ <br/>
  * @desc 将有序数组转换为二叉搜索树 <br/>
- * 将一个按照升序排列的有序数组，转换为一棵高度平衡二叉搜索树。 <br/>
- * 一个高度平衡二叉树是指一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过 1。
+ *       将一个按照升序排列的有序数组，转换为一棵高度平衡二叉搜索树。 <br/>
+ *       一个高度平衡二叉树是指一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过 1。
  */
 public class SortedArrayToBST {
-	
+
 	/**
 	 * @param nums
 	 * @return
 	 */
 	public TreeNode go(int[] nums) {
 		if (nums != null && nums.length > 0) {
-			return build(nums, 0, nums.length - 1);			
+			return build(nums, 0, nums.length - 1);
 		}
 		return null;
-    }
-	
+	}
+
 	private TreeNode build(int[] nums, int left, int right) {
-		if (left > right) return null;
+		if (left > right)
+			return null;
 		int i = (left + right) / 2;
 		TreeNode node = new TreeNode(nums[i]);
 		node.left = build(nums, left, i - 1);
--- src/main/java/learn/from/leetcode/tree/SymmetricBinaryTree.java
+++ src/main/java/learn/from/leetcode/tree/SymmetricBinaryTree.java
@@ -1,10 +1,14 @@
 package learn.from.leetcode.tree;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 import learn.from.leetcode.ds.TreeNode;
 
 /**
  * @author nivance
- * @from https://leetcode-cn.com/problems/symmetric-tree/
+ * @from https://leetcode-cn.com/problems/symmetric-tree/ <br/>
+ *       https://leetcode-cn.com/problems/dui-cheng-de-er-cha-shu-lcof
  * @desc 给定一个二叉树，检查它是否是镜像对称的。
  */
 public class SymmetricBinaryTree {
@@ -27,22 +31,25 @@ public class SymmetricBinaryTree {
 		}
 	}
 
-	// public boolean isSymmetric(TreeNode root) {
-	// Queue<TreeNode> q = new LinkedList<>();
-	// q.add(root);
-	// q.add(root);
-	// while (!q.isEmpty()) {
-	// TreeNode t1 = q.poll();
-	// TreeNode t2 = q.poll();
-	// if (t1 == null && t2 == null) continue;
-	// if (t1 == null || t2 == null) return false;
-	// if (t1.val != t2.val) return false;
-	// q.add(t1.left);
-	// q.add(t2.right);
-	// q.add(t1.right);
-	// q.add(t2.left);
-	// }
-	// return true;
-	// }
+	public boolean isSymmetric2(TreeNode root) {
+		Queue<TreeNode> q = new LinkedList<>();
+		q.add(root);
+		q.add(root);
+		while (!q.isEmpty()) {
+			TreeNode t1 = q.poll();
+			TreeNode t2 = q.poll();
+			if (t1 == null && t2 == null)
+				continue;
+			if (t1 == null || t2 == null)
+				return false;
+			if (t1.val != t2.val)
+				return false;
+			q.add(t1.left);
+			q.add(t2.right);
+			q.add(t1.right);
+			q.add(t2.left);
+		}
+		return true;
+	}
 
 }
