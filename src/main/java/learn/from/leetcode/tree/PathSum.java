b719bc17c0e2ecd3c30748b48b26baacb0086e9d
nivance <limingjian110@163.com>
2020-04-29 15:32:30 +0800

    add path-sum-iii

--- src/main/java/learn/from/leetcode/tree/PathSum.java
+++ src/main/java/learn/from/leetcode/tree/PathSum.java
@@ -21,4 +21,42 @@ public class PathSum {
 		}
 	}
 
+	/**
+	 * --------------------------------------------------
+	 * 
+	 * @from https://leetcode-cn.com/problems/path-sum-iii/
+	 * @desc 给定一个二叉树，它的每个结点都存放着一个整数值。 <br/>
+	 *       找出路径和等于给定数值的路径总数。<br/>
+	 *       路径不需要从根节点开始，也不需要在叶子节点结束，但是路径方向必须是向下的（只能从父节点到子节点）。<br/>
+	 *       二叉树不超过1000个节点，且节点数值范围是 [-1000000,1000000] 的整数。<br/>
+	 * 
+	 *       --------------------------------------------------
+	 */
+	private int count = 0;
+
+	public int pathSum(TreeNode root, int sum) {
+		preOrderPath(root, sum);
+		return count;
+	}
+
+	private void preOrderPath(TreeNode root, int sum) {
+		if (root != null) {
+			count += hasPathSum2(root, sum);
+			if (root.left != null) {
+				preOrderPath(root.left, sum);
+			}
+			if (root.right != null) {
+				preOrderPath(root.right, sum);
+			}
+		}
+	}
+
+	private int hasPathSum2(TreeNode root, int sum) {
+		if (root != null) {
+			int v = sum - root.val;
+			int result = v == 0 ? 1 : 0;
+			return result + hasPathSum2(root.left, v) + hasPathSum2(root.right, v);
+		}
+		return 0;
+	}
 }
